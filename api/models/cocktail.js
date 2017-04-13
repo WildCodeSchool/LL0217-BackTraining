3a822bd7f5b734638e52aec22a88ba50d713372d
nailletine <nailletine.lajoie19@gmail.com>
2017-04-13 19:07:46 +0200

    training Nailletine

--- api/models/cocktail.js
+++ api/models/cocktail.js
@@ -1,7 +1,24 @@
 import mongoose from 'mongoose';
 
 const cocktailSchema = new mongoose.Schema({
-  // Write schema here.
+
+
+name: {
+  type: String
+},
+ingredients: {
+  type: String
+},
+methode: {
+  type: String
+},
+create_at: {
+  type: Date, default: Date.now
+},
+update_at: {
+  type: Date, default: Date.now
+}
+
 });
 
 let model = mongoose.model('Cocktail', cocktailSchema);
@@ -17,5 +34,68 @@ export default class Cocktail {
       }
     });
   }
+
+  create(req, res){
+    model.create(req.body, (err,cocktail) => {
+      if (err){
+        res.status(500).send(err.message);
+      } else {
+        res.json({
+          recettes: cocktail
+        });
+      }
+
+    });
+
+    }
+findById(req, res){
+  model.findById({_id: req.params.id}, (err, id)/*parametre d'erreur, et parametre de reponse*/ => {
+    if (err || !id) {
+      res.sendStatus(500);
+    } else {
+      res.json(id);
+    }});
+    }
+
+    findByName(req, res){
+      model.find({name: req.params.name} ,(err, name)=> {
+        if (err || !name) {
+          res.sendStatus(500);
+        } else {
+          res.json(name);
+        }
+      });
+  }
+    updateOne(req, res){
+      model.update({_id:req.params.id
+      },req.body, (err, cocktails) =>{
+        if (err || !cocktails) {
+          res.status(500).send(err.message);
+        }else {res.sendStatus(200);
+        }
+      }
+    );
+    }
+    findByMethode(req, res){
+      model.find({methode:req.params.methode
+      } ,(err, methode)=> {
+        if (err || !methode) {
+          res.sendStatus(500);
+        } else {
+          res.json(methode);
+        }
+      });
+  }
+  delete(req, res) {
+
+    model.findByIdAndRemove(req.params.id, (err) => {
+      if (err) {
+          res.status(500).send(err.message);
+      } else {
+          res.sendStatus(200);
+      }
+    });
+  }
+
 
 }
--- api/routes/coktails.js
+++ api/routes/coktails.js
@@ -7,8 +7,26 @@ module.exports = (app) => {
 
     var cocktail = new Cocktail();
 
+
+
+
+    router.put('/:id',cocktail.updateOne);
+    //
+   router.get('/methode/:methode',cocktail.findByMethode);
+
+   //router.get('/ingredients/:ingredients',cocktail.findByIngredient);
+    //
+    router.get('/name/:name',cocktail.findByName);
+
+    router.get('/:id',cocktail.findById);//on met ':' car id est variable
+
     router.get('/', cocktail.findAll);
-    
+
+    router.delete('/:id',cocktail.delete);
+
+    router.post('/',cocktail.create);
+
+
     app.use('/cocktails', router);
 
 };
