d21e9a7ffcd895504aeef010a4dbdcb4faa97e27
DeRosario <davidveiga.pereira@gmail.com>
2017-04-13 16:48:55 +0200

    Travail Back de David

--- api/models/cocktail.js
+++ api/models/cocktail.js
@@ -2,11 +2,101 @@ import mongoose from 'mongoose';
 
 const cocktailSchema = new mongoose.Schema({
   // Write schema here.
+
+
+name: {
+  type: String
+},
+ingredients: {
+  type: String
+},
+method: {
+  type: String
+},
+created_at: {
+  type: Date,
+  default: Date.now
+},
+
+updated_at: {
+  type: Date,
+  default: Date.now
+}
+
 });
+
 
 let model = mongoose.model('Cocktail', cocktailSchema);
 
 export default class Cocktail {
+
+
+
+  create(req, res) {
+          model.create(req.body, (err, cocktail) => {
+              if (err) {
+                  res.status(500).send(err.message);
+              } else {
+                  res.json({
+                      success: true,
+                      Listcocktail: cocktail
+                  });
+              }
+          });
+      }
+
+
+      updateOne(req, res) {
+            model.update({
+                name: req.params._id              // C'est ce que tu recherches =>  _id : req.params.id
+            }, req.body, (err, cocktail) => {          // Les champs que tu modifies => req.body
+                if (err) {
+                    res.status(500).send(err.message);
+                } else {
+                        res.sendStatus(200);
+                }
+            });
+        }
+
+      deleteOne(req, res) {
+      model.findByIdAndRemove(req.params._id, (err) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.sendStatus(200);
+        }
+      });
+    }
+
+      findId(req, res) {
+            model.find({_id/*ingredients correspond au schéma*/: req.params._id/*.ingredients correspond au /:ingredients*/}, (err,id) => {
+              if (err) {
+                  res.status(403);
+              } else {
+                  res.json(id);
+              }
+            });
+          }
+
+      findIngredients(req, res) {
+            model.find({ingredients/*ingredients correspond au schéma*/: req.params.ingredients/*.ingredients correspond au /:ingredients*/}, (err,ingredients) => {
+              if (err) {
+                  res.status(403);
+              } else {
+                  res.json(ingredients);
+              }
+            });
+          }
+
+      findName(req, res) {
+            model.find({name/*name correspond au schéma*/: req.params.name}, (err,cocktail) => {
+              if (err) {
+                  res.status(403);
+              } else {
+                  res.json(cocktail);
+              }
+            });
+          }
 
   findAll(req, res) {
     model.find({}, (err, cocktails) => {
@@ -17,5 +107,7 @@ export default class Cocktail {
       }
     });
   }
+
+
 
 }
--- api/routes/coktails.js
+++ api/routes/coktails.js
@@ -7,8 +7,20 @@ module.exports = (app) => {
 
     var cocktail = new Cocktail();
 
+    router.put('/update/:_id', cocktail.updateOne);
+
+    router.delete('/delete/:_id', cocktail.deleteOne);
+
+    router.get('/name/:name', cocktail.findName);
+
+    router.get('/ingredients/:ingredients', cocktail.findIngredients);
+
+    router.get('/id/:_id', cocktail.findId);
+
+    router.post('/', cocktail.create);
+
     router.get('/', cocktail.findAll);
-    
+
     app.use('/cocktails', router);
 
 };
