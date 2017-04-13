efab2f9ea3044fa63c556cf2035d86991596fdb6
peche56 <c.filleul56@gmail.com>
2017-04-13 09:34:39 +0200

    first commit

--- api/models/cocktail.js
+++ api/models/cocktail.js
@@ -1,21 +1,102 @@
 import mongoose from 'mongoose';
 
 const cocktailSchema = new mongoose.Schema({
-  // Write schema here.
+
+    name: {
+        type: String
+    },
+    ingredients: [{
+        type: String
+    }],
+    method: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+
+        type: Date,
+        default: Date.now
+    }
 });
 
 let model = mongoose.model('Cocktail', cocktailSchema);
 
 export default class Cocktail {
 
-  findAll(req, res) {
-    model.find({}, (err, cocktails) => {
-      if (err || !cocktails) {
-        res.sendStatus(403);
-      } else {
-        res.json(cocktails);
-      }
-    });
-  }
+    findAll(req, res) {
+        model.find({}, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    post(req, res) {
+
+        model.create({}, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    findById(req, res) {
+
+        model.find({
+            _id
+        }, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    findByName(req, res) {
+
+        model.find({
+            _name
+        }, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    findByIngredients(req, res) {
+
+        model.find([{
+            _ingredients
+        }], (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    put(req, res) {
+
+        model.update({
+            _id: req.params.id
+        }, req.body, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
 
 }
--- api/routes/coktails.js
+++ api/routes/coktails.js
@@ -8,7 +8,13 @@ module.exports = (app) => {
     var cocktail = new Cocktail();
 
     router.get('/', cocktail.findAll);
-    
+    router.post('/', cocktail.post);
+    router.get('/', cocktail.findById);
+    router.get('/', cocktail.findByName);
+    router.get('/', cocktail.findByIngredients);
+    router.put('/', cocktail.put);
+
+
     app.use('/cocktails', router);
 
 };
