5500aef59b0561f0bdd60a01eb91dfec04be287e
Wolfy <s.leheup@gmail.com>
2017-04-13 11:54:49 +0200

    FindAll, findOne, findById, update, post and delete are done

--- api/models/cocktail.js
+++ api/models/cocktail.js
@@ -1,21 +1,84 @@
 import mongoose from 'mongoose';
 
 const cocktailSchema = new mongoose.Schema({
-  // Write schema here.
+    name: String,
+    ingredients: [String],
+    method: [String],
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 let model = mongoose.model('Cocktail', cocktailSchema);
 
 export default class Cocktail {
 
-  findAll(req, res) {
-    model.find({}, (err, cocktails) => {
-      if (err || !cocktails) {
-        res.sendStatus(403);
-      } else {
-        res.json(cocktails);
-      }
-    });
-  }
+    findAll(req, res) {
+        model.find({}, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    findOne(req, res) {
+        model.findById(req.params.id, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    findByName(req, res) {
+        model.find({
+            name: req.params.name
+        }, (err, cocktails) => {
+            if (err || !cocktails) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    create(req, res) {
+        model.create(req.body, (err, cocktails) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
+
+    update(req, res) {
+      console.log(req.body.name);
+        model.findByIdAndUpdate(req.params.id,req.body, (err, cocktail) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktail);
+            }
+        });
+    }
+
+    delete(req, res) {
+        model.findByIdAndRemove(req.params.id, (err, cocktails) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json(cocktails);
+            }
+        });
+    }
 
 }
--- api/routes/coktails.js
+++ api/routes/coktails.js
@@ -8,7 +8,11 @@ module.exports = (app) => {
     var cocktail = new Cocktail();
 
     router.get('/', cocktail.findAll);
-    
+    router.get('/name/:name', cocktail.findByName);
+    router.get('/:id', cocktail.findOne);
+    router.post('/', cocktail.create);
+    router.put('/:id', cocktail.update);
+    router.delete('/:id', cocktail.delete);
     app.use('/cocktails', router);
 
 };
